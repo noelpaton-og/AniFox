2bcc4a15b18e0f25bb3faa13afda2148364aaac8
Yoshi <yushikamuru@gmail.com>
2025-07-03 17:19:02 -0700

    Fixing the search.js

--- functions/search.js
+++ functions/search.js
@@ -1,28 +1,37 @@
 const axios = require("axios");
 
 exports.handler = async (event) => {
-  const query = event.queryStringParameters.query;
-  const page = event.queryStringParameters.page || 1;
-
-  if (!query) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({ message: "Missing query parameter" }),
-    };
-  }
-
-  const url = `https://api.consumet.org/anime/animefox/${encodeURIComponent(query)}?page=${page}`;
-
   try {
-    const { data } = await axios.get(url);
+    const query = event.queryStringParameters?.query;
+    const page = event.queryStringParameters?.page || 1;
+
+    if (!query) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ error: "Missing 'query' parameter" }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+    }
+
+    const apiUrl = `https://api.consumet.org/anime/animefox/${encodeURIComponent(query)}?page=${page}`;
+    const response = await axios.get(apiUrl);
+
     return {
       statusCode: 200,
-      body: JSON.stringify(data),
+      body: JSON.stringify(response.data),
+      headers: {
+        "Content-Type": "application/json",
+      },
     };
-  } catch (err) {
+  } catch (error) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ message: err.message }),
+      body: JSON.stringify({ error: error.message || "Internal Server Error" }),
+      headers: {
+        "Content-Type": "application/json",
+      },
     };
   }
 };
